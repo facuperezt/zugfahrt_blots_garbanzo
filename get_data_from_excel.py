60752bd268ab79f0d9e3c77186149577eba82bd6
FaQ <facuperezt@googlemail.com>
2023-09-24 18:38:42 +0200

    ready to use

--- get_data_from_excel.py
+++ get_data_from_excel.py
@@ -21,7 +21,7 @@ def convert_rng_to_df(tbl_coords, sheet):
 
     return pd.DataFrame(data_rows, columns=get_column_interval(col_start, col_end))
 
-def concat_tables(ws):
+def concat_tables(ws, lysat_nr):
     ### Dictionary to hold the dfs for each table
     df_dict = {}
 
@@ -34,6 +34,7 @@ def concat_tables(ws):
         df["Kern or Cyt"] = df.columns[0].split(' ')[0]
         df["Date"] = tblname.split('_')[0][-4:]
         df["ID"] = tblname.split('_')[1]
+        df["LysatNr"] = lysat_nr
         df = df.drop(df.index[0])
         # if len(df.columns[0].split(' ')) > 1:
         #     name, _id = df.columns[0].split(' ')
@@ -83,25 +84,32 @@ def apply_normalize_by_K_DL(df: pd.DataFrame, numeric_cols: List[str]) -> pd.Dat
 
 def concatenate_all_sheets(wb) -> pd.DataFrame:
     df = pd.concat(
-        [concat_tables(wb[lysat]) for lysat in ["Lysat "+ str(ind) for ind in range(1,5)]]
+        [concat_tables(wb[lysat], int(lysat.split(' ')[1])) for lysat in ["Lysat "+ str(ind) for ind in range(1,5)]]
     )
     df.index = range(len(df))
     return df
 
 
 def normalize_concatenated_df(df: pd.DataFrame) -> pd.DataFrame:
-    non_numeric_cols = ['Date', 'ID', 'Kern or Cyt', 'Experiment']
+    non_numeric_cols = ['Date', 'ID', 'Kern or Cyt', 'Experiment', 'LysatNr']
     numeric_cols = df.columns.difference(non_numeric_cols)
     df.loc[:, numeric_cols] = df.loc[:, numeric_cols].apply(
-        lambda row: row/row["GAPDH"] if not pd.isna(row["GAPDH"]) else row/row["H3"],
+        lambda row: row/row["H3"] if not pd.isna(row["H3"]) else row/row["GAPDH"],
         axis= 1,
         )  # Normalize by load control
-    df = df.groupby(non_numeric_cols[:-1]).apply(
+    df = df.groupby(non_numeric_cols[:-2]).apply(
         apply_normalize_by_K_DL,
         numeric_cols=numeric_cols,
         )  # Normalize by K DL
     return df
 
+
+def get_p_over_normal_ratio(df: pd.DataFrame) -> pd.DataFrame:
+    get_ratio_from = ["Erk", "Akt", "Stat3"]
+    for protein in get_ratio_from:
+        df[f"p{protein}/{protein}"] = df.loc[:, f"p{protein}"].mean()/df.loc[:, protein].mean()
+
+    return df
 
 
 if __name__ == "__main__":
@@ -109,9 +117,38 @@ if __name__ == "__main__":
     wb = load_workbook(filename)
     df = concatenate_all_sheets(wb)
     norm_df = normalize_concatenated_df(df)
+    norm_df_with_activations = norm_df.reset_index(drop=True).groupby(["LysatNr", "Kern or Cyt", "Experiment", "Date"]).apply(get_p_over_normal_ratio)
 
-tmp = norm_df.reset_index(drop= True).groupby(["Date", "Kern or Cyt", "ID", "Experiment"])\
-.apply(lambda df: df).drop(columns=["Date", "Kern or Cyt", "ID", "Experiment"]).fillna(0).to_numpy()
-plt.imshow(tmp)
+    # tmp = norm_df.reset_index(drop= True).groupby(["Date", "Kern or Cyt", "ID", "Experiment"])\
+    # .apply(lambda df: df).drop(columns=["Date", "Kern or Cyt", "ID", "Experiment"]).fillna(0).to_numpy()
+    # plt.imshow(tmp)
+
+    group_per_exp = norm_df_with_activations.reset_index(drop= True).drop(columns=["Date", "ID"]).groupby(["Kern or Cyt", "Experiment"], group_keys=True)
+    avg_per_exp = group_per_exp.mean()
+    std_per_exp = group_per_exp.std()
+    std_per_exp.drop([ind for ind in avg_per_exp.index if ind[1].startswith("K")], inplace=True)
+    avg_per_exp.drop([ind for ind in avg_per_exp.index if ind[1].startswith("K")], inplace=True)
+    avg_for_plot = avg_per_exp.transpose()
+    std_for_plot = std_per_exp.transpose()
+    avg_for_plot = avg_for_plot.reindex(["Claudin-1", "GAPDH", "H3", "Erk", "Akt", "Stat3", "pErk", "pAkt", "pStat3", "pErk/Erk", "pAkt/Akt", "pStat3/Stat3"])
+    std_for_plot = std_for_plot.reindex(["Claudin-1", "GAPDH", "H3", "Erk", "Akt", "Stat3", "pErk", "pAkt", "pStat3", "pErk/Erk", "pAkt/Akt", "pStat3/Stat3"])
+    sorted_idx = avg_per_exp.values.argsort(axis=0)
+    out_index_name = pd.DataFrame(avg_per_exp.index.to_numpy()[sorted_idx], columns=avg_per_exp.columns)
+    n = 3
+    div, mod = divmod(len(avg_for_plot), n)
+    for kern_or_cyt in ["Kern", "Cytosol"]:
+        fig, axs = plt.subplots(div+mod, n, figsize=(12,10), sharex= True, sharey= False)
+        fig.suptitle(kern_or_cyt)
+        for (idx, mean), (idx, std), ax in zip(avg_for_plot.iterrows(), std_for_plot.iterrows(), axs.flatten()):
+            ax: plt.Axes
+            y_vals = mean.loc[[kern_or_cyt]]
+            x_vals = [ind[1] for ind in y_vals.index]
+            y_err = std.loc[[kern_or_cyt]]
+            ax.bar(x_vals, y_vals)
+            ax.errorbar(x_vals, y_vals, y_err, fmt='x', barsabove= True, ecolor="red", capsize=4)
+            ax.set_xticks(x_vals, x_vals, rotation=45)
+            ax.set_title(idx)
+    plt.show()
+
 
 # %%
